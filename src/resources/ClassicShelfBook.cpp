42d11b141811963a01399aa7c1b5121baa49da2a
Hiba Zia <hibazia307@gmail.com>
2026-04-17 17:28:54 +0500

    add book type

--- src/resources/ClassicShelfBook.cpp
+++ src/resources/ClassicShelfBook.cpp
@@ -1,1 +1,22 @@
+#include "ClassicShelfBook.h"
+#include <iostream>
 
+ClassicShelfBook::ClassicShelfBook(int id, string title, string author, string category)
+    : Resource(id, title, author, category) {}
+
+void ClassicShelfBook::displayDetails()
+{
+    cout << "Classic Shelf Book\n";
+    cout << "Title: " << title << endl;
+    cout << "Author: " << author << endl;
+}
+
+int ClassicShelfBook::getBorrowLimit()
+{
+    return 2;
+}
+
+double ClassicShelfBook::getFineRate()
+{
+    return 3.0;
+}
--- src/resources/ClassicShelfBook.h
+++ src/resources/ClassicShelfBook.h
@@ -1,1 +1,16 @@
+#ifndef CLASSICSHELFBOOK_H
+#define CLASSICSHELFBOOK_H
 
+#include "Resource.h"
+
+class ClassicShelfBook : public Resource
+{
+public:
+    ClassicShelfBook(int id, string title, string author, string category);
+
+    void displayDetails();
+    int getBorrowLimit();
+    double getFineRate();
+};
+
+#endif
