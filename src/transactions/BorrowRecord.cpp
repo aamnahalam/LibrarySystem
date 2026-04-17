c17140bec3a6a9d77651b1fb93a76294d97fd7c5
Aamnah <saamnahalam@gmail.com>
2026-04-17 17:07:53 +0500

    Updated BorrowRecord with full logic

--- src/transactions/BorrowRecord.cpp
+++ src/transactions/BorrowRecord.cpp
@@ -1,16 +1,48 @@
 #include "BorrowRecord.h"
+#include "User.h"
 #include <iostream>
 using namespace std;
 
-BorrowRecord::BorrowRecord(User* user, string resourceName, string borrowDate) {
+BorrowRecord::BorrowRecord(User* user, string resourceName, string borrowDate, string dueDate) {
     this->user = user;
     this->resourceName = resourceName;
     this->borrowDate = borrowDate;
+    this->dueDate = dueDate;
+    this->isReturned = false;
+}
+
+void BorrowRecord::markAsReturned(string returnDate) {
+    this->returnDate = returnDate;
+    this->isReturned = true;
+}
+
+bool BorrowRecord::getReturnStatus() {
+    return isReturned;
+}
+
+string BorrowRecord::getResourceName() {
+    return resourceName;
+}
+
+string BorrowRecord::getBorrowDate() {
+    return borrowDate;
+}
+
+string BorrowRecord::getDueDate() {
+    return dueDate;
 }
 
 void BorrowRecord::showRecord() {
     cout << "User: ";
     user->displayInfo();
-    cout << "Book: " << resourceName << endl;
-    cout << "Date: " << borrowDate << endl;
+
+    cout << "Resource: " << resourceName << endl;
+    cout << "Borrow Date: " << borrowDate << endl;
+    cout << "Due Date: " << dueDate << endl;
+
+    if (isReturned) {
+        cout << "Returned on: " << returnDate << endl;
+    } else {
+        cout << "Status: Not Returned" << endl;
+    }
 }
--- src/transactions/BorrowRecord.h
+++ src/transactions/BorrowRecord.h
@@ -1,18 +1,29 @@
 #ifndef BORROWRECORD_H
 #define BORROWRECORD_H
 
-#include "../users/User.h"
 #include <string>
 using namespace std;
+
+class User; // forward declaration
 
 class BorrowRecord {
 private:
     User* user;
     string resourceName;
     string borrowDate;
+    string dueDate;
+    string returnDate;
+    bool isReturned;
 
 public:
-    BorrowRecord(User* user, string resourceName, string borrowDate);
+    BorrowRecord(User* user, string resourceName, string borrowDate, string dueDate);
+
+    void markAsReturned(string returnDate);
+    bool getReturnStatus();
+
+    string getResourceName();
+    string getBorrowDate();
+    string getDueDate();
 
     void showRecord();
 };
