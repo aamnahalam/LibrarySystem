447afbbe6144c77ff76ce1bea1f1900dedef7c5c
Abdul Rafay Irfan <abrafay0102@gmail.com>
2026-04-21 15:33:39 +0500

    Membership module done

--- src/Membership/NormalMembership.cpp
+++ src/Membership/NormalMembership.cpp
@@ -1,9 +1,23 @@
 #include "NormalMembership.h"
+#include <iostream>
 
-NormalMembership::NormalMembership()
+NormalMembership::NormalMembership() : Membership("Normal")
 {
 }
 
-void NormalMembership::renewMembership()
+int NormalMembership::getMaxBorrowLimit() const
 {
+    return 2;
+}
+
+double NormalMembership::getFineDiscount() const
+{
+    // No discount for Normal Membership
+    return 0.0;
+}
+void NormalMembership::displayDetails() const
+{
+    cout << "Membership: Normal" << endl;
+    cout << "Borrow Limit: 2" << endl;
+    cout << "Fine Discount: 0%" << endl;
 }
