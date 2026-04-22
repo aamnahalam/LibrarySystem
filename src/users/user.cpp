4670bc3483116956463ae71939289a632bc5e986
Abdul Rafay Irfan <abrafay0102@gmail.com>
2026-04-22 16:05:47 +0500

    nothing

--- src/users/user.cpp
+++ src/users/user.cpp
@@ -2,22 +2,24 @@
 #include <iostream>
 using namespace std;
 
-User::User(int id, string firstName, string lastName, string email, string password
-, double balance)
-    : Person(id, firstName, lastName,email,password) {}
+User::User(int id, string firstName, string lastName, string email, string password, double balance)
+    : Person(id, firstName, lastName, email, password) {}
 
 //  Add Balance
-void User::rechargebalance(double amount) {
-    accountbalance+=amount;
+void User::rechargebalance(double amount)
+{
+    accountbalance += amount;
 }
 // Add points
-void User::earnpoints(int points) {
-    loyaltypoints+=points;
+void User::earnpoints(int points)
+{
+    loyaltypoints += points;
 }
 
-void User::displayInfo() {
+void User::displayInfo()
+{
     cout << "User Name: " << firstName << " " << lastName << endl;
-    cout << "Email: " <<email<<endl;
-    cout << "Balance: "<<accountbalance<<endl;
-    cout << "Loyalty Points: " << loyaltypoints<<endl;
+    cout << "Email: " << email << endl;
+    cout << "Balance: " << accountbalance << endl;
+    cout << "Loyalty Points: " << loyaltypoints << endl;
 }
